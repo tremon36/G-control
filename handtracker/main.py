c4f68466ec1914ca56d7f6ac3351c36c6da02688
Richi <tremon36xd@gmail.com>
2021-12-18 17:59:16 +0100

    terminado programa de grabacion de gestos

--- handtracker/main.py
+++ handtracker/main.py
@@ -3,22 +3,44 @@ import collections
 import cv2
 import mediapipe as mp
 import time
+import threading
+import keyboard
+import time
 
+LONGITUD_GRABACION = 20
+recording = LONGITUD_GRABACION + 1
+listaInterna = []
+listaGlobal = []
+
+print("[Advertencia] Si no se pone bien se sobreescriben los datos anteriores.\n Primer indice de archivo de grabacion a utilizar:")
+lastFileIndex = int(input())
+print("Codigo del gesto que se va a grabar: ")
+gestureToRecord = int(input())
+print("pulsar espacio para empezar a grabar. El archivo se guarda automaticamente al pasar el tiempo LONGITUD_GRABACION")
+
+
+
+def pressListerner():
+    while True:
+        if keyboard.is_pressed(' '):
+            global recording
+            global vectors8
+            vectors8 = []
+            recording = 0
+            time.sleep(1)
+        time.sleep(0.05)
+
+
+thread = threading.Thread(target=pressListerner)
+thread.start()
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 192)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 108)
-cap.set(cv2.CAP_PROP_FPS, 10)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-previousTime = 0
-currentTime = 0
-vectors8 = [(0, 0)]
-
-
-iteration = 0
 prevx = 0
 prevy = 0
 
@@ -26,38 +48,45 @@ while True:
 
     success, img = cap.read()
     if success:
+
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handlandmarks in results.multi_hand_landmarks:
-                for id, lm in enumerate(handlandmarks.landmark):
-                    h, w, c = img.shape
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if id == 8:
-                        if iteration < 10:
-                            vectors8.append((x, y))
-                            iteration = iteration + 1
-                        else:
-                            list1 = collections.deque(vectors8)
-                            list1.rotate(-1)
-                            vectors8 = list(list1)
-                            vectors8[len(vectors8)-1] = (x,y)
+        if results.multi_hand_landmarks:  # si hay una mano en la pantalla
+            if recording < LONGITUD_GRABACION:
+                h, w, c = img.shape
+                x0 = 0
+                y0 = 0
+                for handlandmarks in results.multi_hand_landmarks:  # para cada mano
 
-                        i=1
-                        while i < len(vectors8):
-                            cv2.line(img, vectors8[i - 1], vectors8[i], (0, 255, 0), thickness=3)
-                            i = i + 1
-                        print(vectors8)
-                        prevx = x
-                        prevy = y
+                    for id, lm in enumerate(handlandmarks.landmark):  # para cada punto de esa mano
+                        if id == 0:
+                            x0 = lm.x * w
+                            y0 = lm.y * h
+                            if recording == 0:
+                                prevx = x0
+                                prevy = y0
+                            listaInterna.append((x0 - prevx, y0 - prevy))
+                            prevx = x0
+                            prey = y0
+                        elif id == 1 or id == 4 or id == 5 or id == 8 or id == 9 or id == 12 or id == 13 or id == 16 or id == id == 17 or id == 20:
+                            listaInterna.append((lm.x * w - x0, lm.y * h - y0))
 
-                mpDraw.draw_landmarks(img, handlandmarks)
+                    mpDraw.draw_landmarks(img, handlandmarks)  # dibujar los puntos de la mano
+                recording = recording + 1
+                listaGlobal.append(listaInterna)
+                listaInterna = []
+            else:
+                if recording == LONGITUD_GRABACION:
+                    print("Succesfully recorded")
+                    print(listaGlobal)
+                    output = open('output_files/'+str(gestureToRecord) + '_' + str(lastFileIndex),'w')
+                    output.write(str(listaGlobal))
+                    output.close()
+                    listaGlobal = []
+                    recording = recording + 1
+                    lastFileIndex = lastFileIndex + 1
 
-        currentTime = time.time()
-        fps = 1 / (currentTime - previousTime)
-        previousTime = currentTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.imshow("image", img)
+        cv2.imshow("image", cv2.flip(img, 1))
         cv2.waitKey(1)
